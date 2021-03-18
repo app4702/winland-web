4d6cc0fa9303b1b79535e52978c84f43dd612793
serverwinland <app4702@viewstd.com>
2021-03-18 11:25:32 +0800

    網站第一版

---
+++ winland-web/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script src="js/js.js"></script>
+        <style>
+            body,html,*{
+                padding:0px;
+                margin:0px;
+             }
+        </style>
+            
+    </head>
+      <body>
+        <div class="main-page">      <!-- 背景框-->
+          <div id="homedv" style= "height:900px">
+            <div id="top-bar" style= "height:100px; width:100%; position: fixed; background-color: white;">  <!-- 浮動導覽列區塊-->
+              <div class="logo">
+                <img src='./images/logo.png' >
+              </div>
+              <div class="navigation">   <!-- 選單導覽列-->
+                <ul>
+                  <li><a href="#homedv">HOME</a></li>
+                  <li><a href="#aboutdv">ABOUT</a></li>
+                  <li><a href="#servicedv">SERVICE</a></li>
+                  <li><a href="#careersdv">CAREERS</a></li>
+                  <li><a href="#contactdv">CONTACT</a></li>
+                </ul>
+              </div> <!-- 選單導覽列-->
+            </div>   <!-- 浮動導覽列區塊-->
+            <div> 
+              <div class="navigation">   <!-- 導覽列-->
+                <ul>
+                  <li><a href="#homedv">HOME</a></li>
+                  <li><a href="#aboutdv">ABOUT</a></li>
+                  <li><a href="#servicedv">SERVICE</a></li>
+                  <li><a href="#careersdv">CAREERS</a></li>
+                  <li><a href="#contactdv">CONTACT</a></li>
+                </ul>
+              </div> <!-- 導覽列-->
+            </div>                    
+          </div>
+          <div id="aboutdv" style= "height:635px;"></div>
+          <div id="servicedv" style= "height:635px;"></div>
+          <div id="careersdv" style= "height:635px;"></div>
+          <div id="contactdv" style= "height:705px;"></div>
+
+          
+
+
+        </div>                       <!-- 背景框-->
+      </body>
+</html>
