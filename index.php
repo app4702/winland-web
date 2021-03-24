f9cca308027412e615a3b61e2d875b674238f6a7
serverwinland <app4702@viewstd.com>
2021-03-24 15:57:05 +0800

    修改RWD

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title></title>
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script src="js/js.js"></script>
+        <link rel="stylesheet" type="text/css" href="./css/css-mobile.css"/>
+        <link rel="stylesheet" type="text/css" href="./css/css-web.css" media="screen and (min-width:900px)"/>
+        <style>
+            body,html,*{
+                padding:0px;
+                margin:0px;
+             }
+        </style> 
+    </head>
+
+      <body>
+        <div class="main-page">      <!-- 背景框-->
+          <div id="homedv">
+            <div id="top-bar">  <!-- 浮動導覽列區塊-->
+              <div class="logo"></div>
+              <input type="button" class="menu">
+              <div class="navigation">   <!-- 選單導覽列-->
+                <div><ul>
+                  <li><a href="#homedv">HOME</a></li>
+                  <li><a href="#hoteventdv">HOT EVENT</a></li>
+                  <li><a href="#newsdv">NEWS</a></li>
+                  <li><a href="#gamesdv">GAMES</a></li>
+                  <li><a href="#contactdv">CONTACT</a></li>
+                </ul></div>
+              </div> <!-- 選單導覽列-->
+              <div class="mobile-nav"><ul>
+                  <li><a href="#homedv">HOME</a></li>
+                  <li><a href="#hoteventdv">HOT EVENT</a></li>
+                  <li><a href="#newsdv">NEWS</a></li>
+                  <li><a href="#gamesdv">GAMES</a></li>
+                  <li><a href="#contactdv">CONTACT</a></li>
+                </ul></div>
+            </div>   <!-- 浮動導覽列區塊-->
+                           
+          </div>
+          <div id="hoteventdv"></div>
+          <div id="newsdv"></div>
+          <div id="gamesdv"></div>
+          <div id="contactdv"></div>
+          <div id="footerdv"></div>
+        </div>                       <!-- 背景框-->
+      </body>
+</html>
--- js/js.js
+++ js/js.js
@@ -3,14 +3,19 @@ $(function(){
         $(window).bind('scroll resize', function(){
             var $this = $(this);
             var $this_Top=$this.scrollTop();
-            
-            //當高度小於100時，關閉區塊  
             if($this_Top < 100){
-            	$('#top-bar').stop().animate({top:"-100px"});
+                var top_bar = document.getElementById('top-bar');
+                top_bar.style['background-color'] = '';
+                top_bar.style['position'] = '';
             }
             if($this_Top > 100){
-            	$('#top-bar').stop().animate({top:"0px"});
+                var top_bar = document.getElementById('top-bar');
+                top_bar.style['background-color'] = 'white';
+                top_bar.style['position'] = 'fixed';
             }
         }).scroll();
+
+        
     });
 });
+
